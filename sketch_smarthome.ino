9b8babda9c80182fa610284b3594a1e5adab44f8
payl0adK <sanyarfx@yandex.ru>
2021-11-22 18:10:31 +0300

    Added Heat Index Calculation

--- sketch_smarthome.ino
+++ sketch_smarthome.ino
@@ -2,32 +2,32 @@
 
 #define DHTPIN 999999 // номер пина для датчика влажности(DHT11)(Заглушка)
 
-
 DHT dht(DHITPIN, DHT11);
-
 
 void setup() {
   Serial.begin(9600);
-  // Run dht
+  // Инициализация шины SPI
+  // SPI.begin();
   dht.begin();
-
 }
 
 void loop() {
   delay(2000); // Пауза 2 секунды (2000 мс)
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
-  if (isnan(temperature) || isnan(humidity)) {  // Если не удается считать показания, выводится "Ошибка считывания" и программа завершает работу
+  // Heat Index по Цельсию
+  float hic = dht.computeHeatIndex(t, h, false);
+  if (isnan(temperature) || isnan(humidity) || isnan(hic)) {  // Проверка ошибок считывания
     Serial.println("Ошибка считывания с датчика DHT11");
     return;
-  }
-
-  // TODO: HEAT INDEX
-  
+  }  
   // Блок вывода данных в консоль
   Serial.print("Влажность: ");
   Serial.print(humidity);
-  Serial.print(" %\t");
+  Serial.print("%\t");
   Serial.print("Температура: ");
   Serial.print(temperature);
+  Serial.print("%\t");
+  Serial.print("Тепловой индекс: ");
+  Serial.print(hic);
 }
