0d3599a9e85f4aaa14b48eaee970ed3f7c0d2fa6
payl0adK <sanyarfx@yandex.ru>
2021-12-04 15:14:29 +0300

    Added RFID auth

--- sketch_smarthome.ino
+++ sketch_smarthome.ino
@@ -1,34 +1,81 @@
-#include "DHT.h" // библиотека для датчика влажности (DHT sensor library)
+#include "DHT.h" // библиотека для dht11(DHT sensor library)
+#include "MFRC522.h"
+#include "SPI.h"
 
-#define DHTPIN 999999 // номер пина для датчика влажности(DHT11)(Заглушка)
+// #define DHTPIN 2 
 
-DHT dht(DHITPIN, DHT11);
+constexpr uint8_t RST_PIN = 5;
+constexpr uint8_t SS_PIN = 10;
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+//DHT dht(DHTPIN, DHT11);
+
 bool systemIsActive = false;
+String cardUUID = "19 61 77 C2";
 
 void setup() {
   Serial.begin(9600);
-  // Инициализация шины SPI
-  // SPI.begin();
-  dht.begin();
+  SPI.begin();
+  //dht.begin();
+  mfrc522.PCD_Init();
+  mfrc522.PCD_DumpVersionToSerial();
 }
-
+ 
 void loop() {
-  delay(2000); // Пауза 2 секунды (2000 мс)
+  /*delay(2000); // Пауза 2 секунды (2000 мс)
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
   // Heat Index по Цельсию
   float hic = dht.computeHeatIndex(temperature, humidity, false);
-  if (isnan(temperature) || isnan(humidity) || isnan(hic)) {  // Проверка ошибок считывания
+  if (isnan(temperature) || isnan(humidity) || isnan(hic)) {  // Check errors
     Serial.println("Ошибка считывания с датчика DHT11");
     return;
-  }  
+  }  */
+ 
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+ 
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+ 
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message: ");
+  content.toUpperCase();
+  if (content.substring(1) == cardUUID) //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    delay(3000);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    delay(3000);
+  }
+ 
+  
+  /*
   // Блок вывода данных в консоль
   Serial.print("Влажность: ");
   Serial.print(humidity);
-  Serial.print("%\t");
+  Serial.println("%\t");
   Serial.print("Температура: ");
-  Serial.print(temperature);
-  Serial.print("%\t");
+  Serial.println(temperature);
   Serial.print("Тепловой индекс: ");
-  Serial.print(hic);
-}
+  Serial.println(hic);
+  Serial.print("\n");
+  Serial.print("\n");
+  */
+} 
